1d3b6071b019ffbcc947b7ba10aa8b23ec7784f4
Paul- Emmanuel <paultchoume905@gmail.com>
2025-10-31 14:46:05 +0000

    Better styles applied

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Selecting elements
+const noteTitleInput = document.getElementById("noteTitle");
+const searchInput = document.getElementById("searchInput");
+const noteItems = document.querySelector("#note-list-sidebar");
+const textArea = document.getElementById("noteContentArea");
+const newNoteButton = document.getElementById("newNoteButton");
+const saveButton = document.getElementById("saveNoteButton");
+const deleteButton = document.getElementById("deleteNoteButton");
+const closeButton = document.getElementById("closeNoteButton");
